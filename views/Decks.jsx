412bd89510aa62401b03d0dc9ca872d48e680deb
Igor Poluektov <igorpoluektov74@gmail.com>
2023-02-09 17:43:13 +0300

    toDev

--- views/Decks.jsx
+++ views/Decks.jsx
@@ -1,13 +1,13 @@
-const React = require('react')
+const React = require('react');
 
 function Decks() {
   return (
-    <div className="">
-      <a>Тема1</a>
-      <a>Тема2</a>
-      <a>Тема3</a>
+    <div className="deckBtn">
+      <button>Тема1</button>
+      <button>Тема2</button>
+      <button>Тема3</button>
     </div>
-  )
+  );
 }
 
-module.exports = Decks
+module.exports = Decks;
