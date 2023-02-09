26fd609fd1f38e705f38b13859ee7365255c7370
Alisa Savorskaya <alyssen95@mail.ru>
2023-02-09 15:44:43 +0300

    created models db

---
+++ db/migrations/20230209124040-create-deck.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Decks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      theme: {
+        type: Sequelize.TEXT
+      },
+      count_of_cards: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Decks');
+  }
+};
