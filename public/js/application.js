78ccd93c32b287f6985b3184c53d0752af00d452
Igor Poluektov <igorpoluektov74@gmail.com>
2023-02-10 08:54:20 +0300

    redir

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 //TODO редирект на слеш при нажатии на home
 const redirectBtn = document.querySelector('.redirectBtn');
 const deckBtn = document.querySelector('.deckBtn');
+const skipBtn = document.querySelector('.skipBtn');
 
 redirectBtn.addEventListener('click', async (event) => {
   const response = await fetch('/'); //тут должен быть редирект на выбор тем
@@ -8,9 +9,21 @@ redirectBtn.addEventListener('click', async (event) => {
 
 deckBtn.addEventListener('click', async (event) => {
   const target = event.target.getAttribute('id');
-  
   console.log(target);
   window.location.href = `http://localhost:3000/card/${target}`;
+  // const response = await fetch(
+  //   `/card/${target}`
+  //   //тут темы заменяются на вопрос
+  // );
+  // const inp = await response.text();
+  // console.log(inp);
+});
+
+skipBtn.addEventListener('click', async (event) => {
+  const target = event.target.getAttribute('id');
+  target += 1;
+  console.log(target);
+  // window.location.href = `http://localhost:3000/card/:id/${target}`;
   // const response = await fetch(
   //   `/card/${target}`
   //   //тут темы заменяются на вопрос
--- views/CardView.jsx
+++ views/CardView.jsx
@@ -8,6 +8,9 @@ function CardView({ question, answer }) {
         <div className="card-body">
           <h5 className="card-title">Название темы</h5>
           <p className="card-text">Вопрос</p>
+          <button className="skipBtn" id="0">
+            Skip
+          </button>
         </div>
       </div>
     </Home>
