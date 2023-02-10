1d66bd877200831b79f5aae71e80be68f307f67a
Alisa Savorskaya <alyssen95@mail.ru>
2023-02-10 11:02:10 +0300

    added input and more styles

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
 const CardView = require('../views/CardView');
-const { Card } = require('../db/models');
+const { Card, Deck } = require('../db/models');
 
 router.route('/:id1/1').get(async (req, res) => {
   const { id1 } = req.params;
+  const { theme } = await Deck.findOne({ where: { id: id1 } });
   const { question, answer } = await Card.findOne({
     where: { deck_id: id1, id: 1 },
   });
-  res.renderComponent(CardView, { question });
+  res.renderComponent(CardView, { theme, question });
 });
 
 router.route('/:id/:quest').get(async (req, res) => {
   const { id, quest } = req.params;
-  if ()
   const { question, answer } = await Card.findOne({
     where: { id: quest, deck_id: id },
   });
@@ -24,7 +24,7 @@ router.route('/:id/:quest').get(async (req, res) => {
   // const cardQuest = card.question;
   // const cardAnsw = card.answer;
   // res.renderComponent(CardView, { question }, { doctype: false });
-  res.send(question)
+  res.send(question);
 });
 
 module.exports = router;
