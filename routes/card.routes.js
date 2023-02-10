e20d0a12c884521a04df83e29189a854b5a64c14
Igor Poluektov <igorpoluektov74@gmail.com>
2023-02-10 11:02:49 +0300

    beforeStyles

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -12,19 +12,10 @@ router.route('/:id1/1').get(async (req, res) => {
 
 router.route('/:id/:quest').get(async (req, res) => {
   const { id, quest } = req.params;
-  if ()
   const { question, answer } = await Card.findOne({
     where: { id: quest, deck_id: id },
   });
-  console.log(req.params);
-  console.log(question);
-  // await res.text('123');
-  // здесь отрисовывается вопрос по выбранной теме
-  // const cardId = card.id;
-  // const cardQuest = card.question;
-  // const cardAnsw = card.answer;
-  // res.renderComponent(CardView, { question }, { doctype: false });
-  res.send(question)
+  res.send(question);
 });
 
 module.exports = router;
